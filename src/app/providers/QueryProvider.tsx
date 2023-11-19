81160b87c32596167ad03186242c7a011deec0ec
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2023-11-19 21:58:30 +0100

    feat: wrap body into QueryProvider

---
+++ src/app/providers/QueryProvider.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactNode, useState } from 'react'
+
+function QueryProvider({ children }: { children: ReactNode }) {
+
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+export default QueryProvider
